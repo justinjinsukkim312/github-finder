201d8d3db25c1a5f8a88908b0f81db5d0713262e
jinsukmik <jinsukmik@gmail.com>
2019-06-25 00:09:33 -0500

    components and data

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ class App extends Component {
 		users: [],
 		user: {},
 		loading: false,
-		alert: null
+		alert: null,
+		repos: []
 	};
 	// * DEFAULT LOADED SEARCH LIST *
 	// async componentDidMount() {
@@ -48,6 +49,16 @@ class App extends Component {
 		);
 		this.setState({ user: res.data, loading: false });
 	};
+	// get users repos
+	getUserRepos = async username => {
+		this.setState({ loading: true });
+		const res = await axios.get(
+			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
+				process.env.REACT_APP_GITHUB_CLIENT_ID
+			}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		this.setState({ repos: res.data, loading: false });
+	};
 	// clear users
 	clearUsers = () => this.setState({ users: [], loading: false });
 	// set alert
@@ -56,7 +67,7 @@ class App extends Component {
 		setTimeout(() => this.setState({ alert: null }), 5000);
 	};
 	render() {
-		const { users, user, loading } = this.state;
+		const { users, user, loading, repos } = this.state;
 		return (
 			<Router>
 				<div className="App">
@@ -87,6 +98,8 @@ class App extends Component {
 									<User
 										{...props}
 										getUser={this.getUser}
+										getUserRepos={this.getUserRepos}
+										repos={repos}
 										user={user}
 										loading={loading}
 									/>
