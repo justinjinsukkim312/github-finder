d5a31f8e7c28d824d85c7c3a7f628dcc79a2c762
jinsukmik <jinsukmik@gmail.com>
2019-06-24 20:48:30 -0500

    github api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-	// vanilla js
-	// return React.createElement('div', { className:'App'}, React.createElement('h1', null, 'Hello from React'));
+	state = {
+		users: [],
+		loading: false
+	};
+	async componentDidMount() {
+		this.setState({ loading: true });
+		const res = await axios.get('https://api.github.com/users');
+		this.setState({ users: res.data, loading: false });
+	}
+
 	render() {
 		return (
 			<div className="App">
 				<Navbar />
-				<UserItem />
+				<div className="container">
+					<Users loading={this.state.loading} users={this.state.users} />
+				</div>
 			</div>
 		);
 	}
