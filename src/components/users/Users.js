5a5420807c847f3bfdecedad639a4c3796624a71
jinsukmik <jinsukmik@gmail.com>
2019-06-24 20:57:50 -0500

    proptypes

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+
 const Users = ({ users, loading }) => {
 	if (loading) {
 		return <Spinner />;
@@ -14,6 +16,12 @@ const Users = ({ users, loading }) => {
 		);
 	}
 };
+
+Users.propTypes = {
+	users: PropTypes.array.isRequired,
+	loading: PropTypes.bool.isRequired
+};
+
 const userStyle = {
 	display: 'grid',
 	gridTemplateColumns: 'repeat(3, 1fr)',
